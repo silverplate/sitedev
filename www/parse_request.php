b7e1cec068a2eef75b68175e2f0130895ace6e86
Kirill Brusenskiy <kirill@brusenskiy.ru>
2011-08-10 16:01:21 +0400

    Forgotten update

--- www/parse_request.php
+++ www/parse_request.php
@@ -1,59 +1,66 @@
 <?php
 
-require_once('prepend.php');
+require_once 'prepend.php';
 
-$g_cache = '' == SITE_LANG
-	? new ProjectCache()
-	: new ProjectCache(null, SITE_LANG);
 
-if ($g_cache->IsAvailable() && $g_cache->IsCache()) {
-	echo $g_cache;
+$gCache = '' == SITE_LANG
+        ? new ProjectCache()
+        : new ProjectCache(null, SITE_LANG);
+
+if ($gCache->isAvailable() && $gCache->isCache()) {
+    echo $gCache;
 
 } else {
-	$real_url = parse_url($_SERVER['REQUEST_URI']);
-	$url = parse_url(get_custom_url($_SERVER['REQUEST_URI']));
-	$document = Document::Load($url['path'], 'uri');
+    $realUrl = parse_url($_SERVER['REQUEST_URI']);
+    $url = parse_url(getCustomUrl($_SERVER['REQUEST_URI']));
+    $document = Document::load($url['path'], 'uri');
 
-	if ($document && $document->GetAttribute('link') && $document->GetAttribute('link') != $real_url['path']) {
-		goToUrl($document->GetAttribute('link'));
+    if (
+        $document &&
+        $document->getAttribute('link') &&
+        $document->getAttribute('link') != $realUrl['path']
+    ) {
+        goToUrl($document->getAttribute('link'));
 
-	} else {
-		if (
-			$document && $document->GetHandler() &&
-			($document->GetAttribute('is_published') == 1 || IS_SHOW_HIDDEN) &&
-			(!$document->GetAttribute('auth_status_id') || is_null(User::GetAuthGroup()) || $document->GetAttribute('auth_status_id') & User::GetAuthGroup())
-		) {
-			$handler = Document::InitHandler($document->GetHandlerFile(), $document);
-			$handler->Execute();
-			$handler->Output();
-		} else {
-			document_not_found();
-		}
-	}
+    } else {
+        if (
+            $document &&
+            $document->getHandler() &&
+            ($document->getAttribute('is_published') == 1 || IS_SHOW_HIDDEN) &&
+            (!$document->getAttribute('auth_status_id') || is_null(User::getAuthGroup()) || $document->getAttribute('auth_status_id') & User::getAuthGroup())
+        ) {
+            $handler = Document::initHandler($document->getHandler(), $document);
+            $handler->execute();
+            $handler->output();
+
+        } else {
+            documentNotFound();
+        }
+    }
 }
 
 
-function get_custom_url($_url) {
-	global $g_custom_urls;
+function getCustomUrl($_url)
+{
+    global $gCustomUrls;
 
-	$url = '' != SITE_LANG && 0 === strpos($_url, '/' . SITE_LANG . '/')
-		? substr($_url, strlen(SITE_LANG) + 1)
-		: $_url;
 
-	if (empty($g_custom_urls)) {
-		return $url;
+    $url = '' != SITE_LANG && 0 === strpos($_url, '/' . SITE_LANG . '/')
+         ? substr($_url, strlen(SITE_LANG) + 1)
+         : $_url;
 
-	} else {
-		$urls = $g_custom_urls;
-		array_walk($urls, 'url_escape');
-		preg_match('/^\/(' . implode('|', $urls) . ')\//', $url, $matches);
+    if (empty($gCustomUrls)) {
+        return $url;
 
-		return (SITE_LANG ? '/' . SITE_LANG : '') . ($matches ? $matches[0] : $url);
-	}
+    } else {
+        $urls = $gCustomUrls;
+        array_walk($urls, 'escapeUrl');
+        preg_match('/^\/(' . implode('|', $urls) . ')\//', $url, $matches);
+        return (SITE_LANG ? '/' . SITE_LANG : '') . ($matches ? $matches[0] : $url);
+    }
 }
 
-function url_escape(&$_item) {
-	$_item = preg_replace('/(\/|\-)/', '\\\$1', $_item);
+function escapeUrl(&$_item)
+{
+    $_item = preg_replace('/(\/|\-)/', '\\\$1', $_item);
 }
-
-?>

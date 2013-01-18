2728a867d47bd3de957fc704e283dfa08390a03e
Kirill Brusenskiy <kirill@brusenskiy.ru>
2013-01-19 03:25:15 +0400

    Обновления для работы на сервере на Windows.

--- www/cms/prepend.php
+++ www/cms/prepend.php
@@ -4,9 +4,9 @@ implementFile(__FILE__, 1);
 
 function implementFile($_localFilePath, $_depthInsideDocRoot)
 {
-    $path = explode(DIRECTORY_SEPARATOR, ltrim($_localFilePath, DIRECTORY_SEPARATOR));
+    $path = explode(DIRECTORY_SEPARATOR, $_localFilePath);
     $pos = count($path) - $_depthInsideDocRoot - 2;
     array_splice($path, $pos, 1, array('core', $path[$pos]));
 
-    require_once '/' . implode(DIRECTORY_SEPARATOR, $path);
+    require_once implode(DIRECTORY_SEPARATOR, $path);
 }

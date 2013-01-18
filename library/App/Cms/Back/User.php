5b005956ddaffbcd9f2fda0a97c1e618aaa954eb
Kirill Brusenskiy <kirill@brusenskiy.ru>
2013-01-18 13:22:30 +0400

    Обновление ядра: новое шифрование, новый установщик.

--- library/App/Cms/Back/User.php
+++ library/App/Cms/Back/User.php
@@ -2,4 +2,5 @@
 
 class App_Cms_Back_User extends Core_Cms_Back_User
 {
+    const SECRET = '$2y$09$d42R0c216184E3f0tjm60c';
 }
--- library/App/Cms/User.php
+++ library/App/Cms/User.php
@@ -2,4 +2,5 @@
 
 class App_Cms_User extends Core_Cms_User
 {
+    const SECRET = '$2y$09$d42R0c216184E3f0tjm60c';
 }

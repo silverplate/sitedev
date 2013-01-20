0139305ba6ceb082e2cc2ff08be03ccbe66a89be
Kirill Brusenskiy <kirill@brusenskiy.ru>
2013-01-21 01:50:21 +0400

    Обновление: страницы ошибок.

--- library/App/Error.php
+++ library/App/Error.php
@@ -2,4 +2,11 @@
 
 class App_Error extends Core_Error
 {
+    public function showUserfriendlyMessage()
+    {
+        $file = TEMPLATES . '500.html';
+
+        if (is_file($file)) readfile($file);
+        else                return parent::showUserfriendlyMessage();
+    }
 }
